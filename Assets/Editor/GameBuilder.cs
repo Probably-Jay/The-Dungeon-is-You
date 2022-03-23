817dec4f380c289327ef8a7a140f7f3d43556c68
Jay <jake.bretherton05@gmail.com>
2022-03-23 20:10:58 +0000

    builder

---
+++ Assets/Editor/GameBuilder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Builder
+{
+    public class GameBuilder : MonoBehaviour
+    {
+        [MenuItem("Build/Build windows")]
+        public static void PerformWindowsBuild() // do not rename
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"};
+            buildPlayerOptions.locationPathName = "build/windows/theDungeonIsYou.exe";
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.LogError("Build failed");
+            }
+            
+
+        }
+    }
+}
